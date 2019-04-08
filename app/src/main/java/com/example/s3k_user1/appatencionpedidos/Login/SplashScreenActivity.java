982a5b6d53b3cd78952f506f381ba7897d00bffd
rogergcc <rogergcc@outlook.com>
2019-04-07 23:23:12 -0500

    pedidos login
    login

---
+++ app/src/main/java/com/example/s3k_user1/appatencionpedidos/Login/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package com.example.s3k_user1.appatencionpedidos.Login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.s3k_user1.appatencionpedidos.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen_background);
+    }
+}
---
+++ app/src/main/java/com/example/s3k_user1/appatencionpedidos/adapter/MoviesRecyclerAdapter.java
@@ -0,0 +1,5 @@
+package com.example.s3k_user1.appatencionpedidos.adapter;
+
+public class MoviesRecyclerAdapter {
+    //https://github.com/raviyadav5951/VolleyWithGsonDemo/tree/15774124a789ee592f686c88ddb9065d63e15c77/app/src/main/java/com/askfortrciks/volleywithgsondemo
+}
